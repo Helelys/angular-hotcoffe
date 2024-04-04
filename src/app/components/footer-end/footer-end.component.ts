f38132fadf41b6bfe75cbab4149ca278779dac69
Helelys <tallyslabanca@outlook.com>
2024-04-04 14:35:37 -0300

    finished footer end

---
+++ src/app/components/footer-end/footer-end.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-end',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-end.component.html',
+  styleUrl: './footer-end.component.css'
+})
+export class FooterEndComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,13 +4,14 @@ import { HeroComponent } from "../../components/hero/hero.component";
 import { ContentComponent } from "../../components/content/content.component";
 import { FooterHeaderComponent } from "../../components/footer-header/footer-header.component";
 import { FooterContentComponent } from "../../components/footer-content/footer-content.component";
+import { FooterEndComponent } from "../../components/footer-end/footer-end.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeaderComponent, HeroComponent, ContentComponent, FooterHeaderComponent, FooterContentComponent]
+    imports: [HeaderComponent, HeroComponent, ContentComponent, FooterHeaderComponent, FooterContentComponent, FooterEndComponent]
 })
 export class HomeComponent {
 
