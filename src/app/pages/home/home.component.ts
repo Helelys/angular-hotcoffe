62bb5c31a961becd23c82cc41afc7ec3c42523a5
Helelys <tallyslabanca@outlook.com>
2024-04-04 14:15:20 -0300

    footer content finished

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,14 @@ import { HeaderComponent } from "../../components/header/header.component";
 import { HeroComponent } from "../../components/hero/hero.component";
 import { ContentComponent } from "../../components/content/content.component";
 import { FooterHeaderComponent } from "../../components/footer-header/footer-header.component";
+import { FooterContentComponent } from "../../components/footer-content/footer-content.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeaderComponent, HeroComponent, ContentComponent, FooterHeaderComponent]
+    imports: [HeaderComponent, HeroComponent, ContentComponent, FooterHeaderComponent, FooterContentComponent]
 })
 export class HomeComponent {
 
