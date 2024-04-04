1649714fc32b4e49cc029af61fc4d0818e2537c4
Helelys <tallyslabanca@outlook.com>
2024-04-04 13:51:25 -0300

    footer header

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { HeroComponent } from "../../components/hero/hero.component";
 import { ContentComponent } from "../../components/content/content.component";
+import { FooterHeaderComponent } from "../../components/footer-header/footer-header.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeaderComponent, HeroComponent, ContentComponent]
+    imports: [HeaderComponent, HeroComponent, ContentComponent, FooterHeaderComponent]
 })
 export class HomeComponent {
 
