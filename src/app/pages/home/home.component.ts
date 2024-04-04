82883df65bf3429bc097c68b83c50947ed3c6b03
Helelys <tallyslabanca@outlook.com>
2024-04-03 23:22:29 -0300

    content and cards finished

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { HeroComponent } from "../../components/hero/hero.component";
+import { ContentComponent } from "../../components/content/content.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeaderComponent, HeroComponent]
+    imports: [HeaderComponent, HeroComponent, ContentComponent]
 })
 export class HomeComponent {
 
